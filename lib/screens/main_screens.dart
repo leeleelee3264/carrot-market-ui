cddca9fc4cca5543f6d2031933174a48e552bf48
leeleelee3264 <absinthe4902@naver.com>
2022-07-27 23:29:22 +0900

    Make general theme

---
+++ lib/screens/main_screens.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class MainScreens extends StatefulWidget {
+  const MainScreens({Key? key}) : super(key: key);
+
+  @override
+  _MainScreensState createState() => _MainScreensState();
+}
+
+class _MainScreensState extends State<MainScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('MainScreens'),
+      ),
+    );
+  }
+}
