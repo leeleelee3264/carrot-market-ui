169ac220c4d177edd67d38fe5afe0f9adad91288
leeleelee3264 <absinthe4902@naver.com>
2022-07-30 10:58:44 +0900

    In the middle of Implement My Carrot Header

--- lib/screens/my_carrot/my_carrot_screen.dart
+++ lib/screens/my_carrot/my_carrot_screen.dart
@@ -1,11 +1,30 @@
+import 'package:carrot_market_ui/screens/my_carrot/components/my_carrot_header.dart';
 import 'package:flutter/material.dart';
 
 class MyCarrotScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('myCarrotScreen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('나의 당근'),
+        actions: [
+          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey),
+        ),
+      ),
+      body: ListView(
+        children: [
+          MyCarrotHeader(),
+          const SizedBox(height: 8.0),
+          const SizedBox(height: 8.0),
+          const SizedBox(height: 8.0),
+        ],
+      ),
     );
   }
 }
