5b20f2b16ef340f18b4495c22866163d76e151fd
leeleelee3264 <absinthe4902@naver.com>
2022-07-30 12:43:44 +0900

    Add Chat

--- lib/screens/chatting/chatting_screen.dart
+++ lib/screens/chatting/chatting_screen.dart
@@ -1,11 +1,24 @@
+import 'package:carrot_market_ui/models/chat_message.dart';
+import 'package:carrot_market_ui/screens/components/appbar_prefreed_size.dart';
+import 'package:carrot_market_ui/screens/components/chat_container.dart';
 import 'package:flutter/material.dart';
 
 class ChattingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ChattingScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('채팅'),
+        bottom: appBarBottomLine(),
+      ),
+
+      body: ListView(
+        children: List.generate(
+          chatMessageList.length,
+            (index) => ChatContainer(chatMessage: chatMessageList[index]),
+        ),
+      ),
     );
   }
 }
