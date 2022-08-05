dc7cf35d3a43cb51482b28f73dd0e3486e59b538
leeleelee3264 <absinthe4902@naver.com>
2022-08-05 15:54:57 +0900

    Implement neighborhood life

--- lib/screens/neigborhood_life/neigborhood_life_screen.dart
+++ lib/screens/neigborhood_life/neigborhood_life_screen.dart
@@ -1,11 +1,42 @@
+import 'package:carrot_market_ui/models/neighborhood_life.dart';
+import 'package:carrot_market_ui/screens/components/appbar_prefreed_size.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'components/life_body.dart';
+import 'components/life_header.dart';
 
 class NeighborhoodLifeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('neigborhoodLifeScreen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('동네생활'),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.search)),
+          IconButton(onPressed: (){},
+              icon: Icon(CupertinoIcons.plus_rectangle_on_rectangle)),
+          IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bell)),
+        ],
+
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          LifeHeader(),
+          ...List.generate(
+              neighborhoodLifeList.length,
+              (index) => Padding(
+                padding: const EdgeInsets.only(bottom: 12.0),
+                child: LifeBody(
+                    neighborhoodLife:neighborhoodLifeList[index]
+                ),
+              )
+          ),
+        ],
+      ),
     );
   }
 }
